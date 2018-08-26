a620c4fb5466ba7c72b90129a8a7aa84e180cbe9
Paul Galbraith <paul.d.galbraith@gmail.com>
2018-08-25 21:41:45 -0400

    spring-boot-cli 2.0.4 initial package build

---
+++ manual/spring-boot-cli/README.md
@@ -0,0 +1,3 @@
+﻿Chocolatey package for Spring Boot CLI.
+
+https://docs.spring.io/spring-boot/docs/current/reference/html/getting-started-installing-spring-boot.html#getting-started-installing-the-cli
---
+++ manual/spring-boot-cli/package.cmd
@@ -0,0 +1,9 @@
+REM Chocolatey and curl must be installed prior to running this packaging script.
+REM To test, "choco install spring-boot-cli -dv -s ."
+
+if not exist tools\spring-boot-cli-2.0.4.RELEASE-bin.zip (
+  curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.0.4.RELEASE/spring-boot-cli-2.0.4.RELEASE-bin.zip ^
+    --output tools\spring-boot-cli-2.0.4.RELEASE-bin.zip
+)
+
+choco pack
