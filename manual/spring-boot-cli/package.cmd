4246dc803ccea02322ecc4f540c3a433145ee80e
Paul Galbraith <paul.d.galbraith@gmail.com>
2018-09-14 18:53:17 -0400

    Spring Boot CLI v2.0.5

--- manual/spring-boot-cli/package.cmd
+++ manual/spring-boot-cli/package.cmd
@@ -1,9 +1,9 @@
 REM Chocolatey and curl must be installed prior to running this packaging script.
 REM To test, "choco install spring-boot-cli -dv -s ."
 
-if not exist tools\spring-boot-cli-2.0.4.RELEASE-bin.zip (
-  curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.0.4.RELEASE/spring-boot-cli-2.0.4.RELEASE-bin.zip ^
-    --output tools\spring-boot-cli-2.0.4.RELEASE-bin.zip
+if not exist tools\spring-boot-cli-2.0.5.RELEASE-bin.zip (
+  curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.0.5.RELEASE/spring-boot-cli-2.0.5.RELEASE-bin.zip ^
+    --output tools\spring-boot-cli-2.0.5.RELEASE-bin.zip
 )
 
 choco pack
--- manual/spring-boot-cli/tools/chocolateyInstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.4.RELEASE"
+$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.5.RELEASE"
 
-Get-ChocolateyUnzip "$toolsDir/spring-boot-cli-2.0.4.RELEASE-bin.zip" $packageDir
-Remove-Item -path $toolsDir/spring-boot-cli-2.0.4.RELEASE-bin.zip
-Install-ChocolateyEnvironmentVariable "SPRING_HOME" $spring_home Machine
-Install-BinFile "spring" "$spring_home/bin/spring.bat"
+Get-ChocolateyUnzip "$toolsDir\spring-boot-cli-2.0.5.RELEASE-bin.zip" "$env:ChocolateyPackageFolder"
+Remove-Item -path "$toolsDir\spring-boot-cli-2.0.5.RELEASE-bin.zip"
+Install-ChocolateyEnvironmentVariable "SPRING_HOME" "$spring_home" Machine
+Install-BinFile "spring" "$spring_home\bin\spring.bat"
