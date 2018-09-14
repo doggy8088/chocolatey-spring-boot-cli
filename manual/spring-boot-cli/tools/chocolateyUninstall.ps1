5d69c0dfcdd94057236a5213af7b09b686d4a64f
Paul Galbraith <paul.d.galbraith@gmail.com>
2018-09-14 18:19:12 -0400

    remove spring cli zip when done installing

--- manual/spring-boot-cli/tools/chocolateyUninstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyUninstall.ps1
@@ -1,6 +1,5 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$packageDir = Split-Path $toolsDir
-$spring_home = Join-Path $packageDir "spring-2.0.4.RELEASE"
+$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.4.RELEASE"
 
 Uninstall-ChocolateyEnvironmentVariable "SPRING_HOME" Machine
 Uninstall-BinFile "spring" "$spring_home/bin/spring.bat"
