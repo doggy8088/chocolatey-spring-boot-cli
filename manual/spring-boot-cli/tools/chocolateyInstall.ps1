5f8dd3567d0105c229a04507c272aacec8396278
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-01-16 20:47:12 -0500

    Spring Boot CLI v2.1.2

--- manual/spring-boot-cli/tools/chocolateyInstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.1.1.RELEASE"
+$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.1.2.RELEASE"
 
-Get-ChocolateyUnzip "$toolsDir\spring-boot-cli-2.1.1.RELEASE-bin.zip" "$env:ChocolateyPackageFolder"
-Remove-Item -path "$toolsDir\spring-boot-cli-2.1.1.RELEASE-bin.zip"
+Get-ChocolateyUnzip "$toolsDir\spring-boot-cli-2.1.2.RELEASE-bin.zip" "$env:ChocolateyPackageFolder"
+Remove-Item -path "$toolsDir\spring-boot-cli-2.1.2.RELEASE-bin.zip"
 Install-ChocolateyEnvironmentVariable "SPRING_HOME" "$spring_home" Machine
 Install-BinFile "spring" "$spring_home\bin\spring.bat"
