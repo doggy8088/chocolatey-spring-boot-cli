6645960eca98e5add2bc192ccfcb6892edb660ad
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-02-15 04:13:53 -0500

    package install file -> Install.ps1

--- manual/fluent-terminal/tools/chocolateyinstall.ps1
+++ manual/fluent-terminal/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $packageZip = "$toolsDir\FluentTerminal.App_0.3.1.0_Test.zip"
 
 Get-ChocolateyUnzip $packageZip $installDir
 Remove-Item $packageZip
-Move-Item -Path "$toolsDir\CustomInstall.ps1" -Destination $installDir
-Start-ChocolateyProcessAsAdmin "& `"$installDir\CustomInstall.ps1`" -Force -ForceContextMenu" -Minimized -ValidExitCodes @(0) 
+Move-Item -Path "$toolsDir\Install.ps1" -Destination $installDir -Force
+Start-ChocolateyProcessAsAdmin "& `"$installDir\Install.ps1`" -Force -ForceContextMenu" -Minimized -ValidExitCodes @(0) 
 Remove-Item $installDir -Recurse
