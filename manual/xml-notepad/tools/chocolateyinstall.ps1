ee493ccef054b72937a01e7e3268fadbb9715858
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-02-16 03:06:59 -0500

    XML Notepad 2.7.1.21

---
+++ manual/xml-notepad/tools/chocolateyinstall.ps1
@@ -0,0 +1,25 @@
+﻿
+$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'xmlnotepadsetup.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'MSI'
+  url           = $url
+  url64bit      = $url64
+  file          = $fileLocation
+
+  softwareName  = 'XML Notepad'
+
+  checksum      = ''
+  checksumType  = 'sha256'
+  checksum64    = ''
+  checksumType64= 'sha256'
+
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
