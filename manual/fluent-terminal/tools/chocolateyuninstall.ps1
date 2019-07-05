6f112e5b8e3443ad5bbe6f02c8b0a40d3afb568d
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-07-04 23:55:24 -0400

    fluent-terminal v0.5.1.0

--- manual/fluent-terminal/tools/chocolateyuninstall.ps1
+++ manual/fluent-terminal/tools/chocolateyuninstall.ps1
@@ -2,6 +2,9 @@ $ErrorActionPreference = 'Stop';
 
 Get-AppxPackage -Name *FluentTerminal* | Remove-AppxPackage -AllUsers
 & certutil -delstore TrustedPeople 1ec0504f23d8f58f4b2cbe942f41d279
+
+$ErrorActionPreference = 'Continue'
 reg delete "HKCU\Software\Classes\Directory\shell\Open Fluent Terminal here" /f
 reg delete "HKCU\Software\Classes\Directory\Background\shell\Open Fluent Terminal here" /f
-reg delete "HKCU\Software\Classes\LibraryFolder\Background\shell\Open Fluent Terminal here" /f
+reg delete "HKCU\Software\Classes\Directory\LibraryFolder\shell\Open Fluent Terminal here" /f
+$ErrorActionPreference = 'Stop'
