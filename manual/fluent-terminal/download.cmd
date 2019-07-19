9433eb2e9c0e8f616a0aa7217082f9af006c6c69
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-07-19 14:26:36 -0400

    fluent-terminal v0.5.2.0

--- manual/fluent-terminal/download.cmd
+++ manual/fluent-terminal/download.cmd
@@ -1,1 +1,1 @@
-curl -L https://github.com/felixse/FluentTerminal/releases/download/0.5.1.0/FluentTerminal.App_0.5.1.0_Test.zip --output tools\FluentTerminal.App_0.5.1.0_Test.zip
+curl -L https://github.com/felixse/FluentTerminal/releases/download/0.5.2.0/FluentTerminal.App_0.5.2.0_Test.zip --output tools\FluentTerminal.App_0.5.2.0_Test.zip
