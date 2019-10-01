3b3f5bfa67009b23b98c66f92223d3a736738099
Thomas P <tpxp@live.fr>
2019-10-01 13:50:29 +0200

    Update Fluent-Terminal to v0.6.0.0
    Some people might prefer to stick with v0.5.3.0 since v0.6.0.0 has
    a critical bug https://github.com/felixse/FluentTerminal/issues/528

--- manual/fluent-terminal/download.cmd
+++ manual/fluent-terminal/download.cmd
@@ -1,1 +1,1 @@
-curl -L https://github.com/felixse/FluentTerminal/releases/download/0.5.3.0/FluentTerminal.App_0.5.3.0_Test.zip --output tools\FluentTerminal.App_0.5.3.0_Test.zip
+curl -L https://github.com/felixse/FluentTerminal/releases/download/0.6.0.0/FluentTerminal.Package_0.6.0.0_Test.zip --output tools\FluentTerminal.App_0.6.0.0_Test.zip
