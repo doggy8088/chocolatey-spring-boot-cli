0f6885c621989e03a1f2e33a2c54f2af23e487c2
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-10-11 10:55:06 -0400

    upgrade spring-boot-cli to v2.1.9

--- manual/spring-boot-cli/download.cmd
+++ manual/spring-boot-cli/download.cmd
@@ -1,2 +1,2 @@
-curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.8.RELEASE/spring-boot-cli-2.1.8.RELEASE-bin.zip --output spring-boot-cli-2.1.8.RELEASE-bin.zip
-7z x spring-boot-cli-2.1.8.RELEASE-bin.zip
+curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.9.RELEASE/spring-boot-cli-2.1.9.RELEASE-bin.zip --output spring-boot-cli-2.1.9.RELEASE-bin.zip
+7z x spring-boot-cli-2.1.9.RELEASE-bin.zip
