fbb109391dc22c13b9e73b66836538713efa8dc5
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-11-20 11:42:18 -0500

    spring-boot-cli v2.2.0

--- manual/spring-boot-cli/tools/chocolateyBeforeModify.ps1
+++ manual/spring-boot-cli/tools/chocolateyBeforeModify.ps1
@@ -1,7 +1,7 @@
 # Remove this release when either upgrading or uninstalling
 
 # use this for a while to clean up older installs
-#$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.1.10.RELEASE"
+#$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.2.0.RELEASE"
 $spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.?.?.RELEASE"
 
 #Start-ChocolateyProcessAsAdmin "Remove-Item `"$spring_home`" -Recurse" -validExitCodes @(0)
