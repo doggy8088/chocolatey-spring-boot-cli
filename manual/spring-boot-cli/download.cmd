2d8241d63d4cddebbc1c20543ca858141ee8b166
Paul Galbraith <paul.d.galbraith@gmail.com>
2019-12-26 00:10:15 -0500

    spring-boot-cli v2.1.11

--- manual/spring-boot-cli/download.cmd
+++ manual/spring-boot-cli/download.cmd
@@ -1,2 +1,2 @@
-curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.2.1.RELEASE/spring-boot-cli-2.2.1.RELEASE-bin.zip --output spring-boot-cli-2.2.1.RELEASE-bin.zip
-7z x spring-boot-cli-2.2.1.RELEASE-bin.zip
+curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.1.11.RELEASE/spring-boot-cli-2.1.11.RELEASE-bin.zip --output spring-boot-cli-2.1.11.RELEASE-bin.zip
+7z x spring-boot-cli-2.1.11.RELEASE-bin.zip
