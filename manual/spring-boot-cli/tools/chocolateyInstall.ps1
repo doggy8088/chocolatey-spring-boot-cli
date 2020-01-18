b8bc0610af554f0e31a2f293522bfef8562bab53
Paul Galbraith <paul.d.galbraith@gmail.com>
2020-01-17 23:17:02 -0500

    spring-boot-cli v1.5.22 fix

--- manual/spring-boot-cli/tools/chocolateyInstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyInstall.ps1
@@ -2,4 +2,4 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $spring_home = Join-Path $env:ChocolateyPackageFolder "spring-1.5.22.RELEASE"
 
 Install-ChocolateyEnvironmentVariable "SPRING_HOME" "$spring_home" Machine
-#Install-BinFile "spring" "$spring_home\bin\spring.bat"
+Install-BinFile "spring" "$spring_home\bin\spring.bat"
--- manual/spring-boot-cli/tools/chocolateyUninstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyUninstall.ps1
@@ -2,4 +2,4 @@ $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
 $spring_home = Join-Path $env:ChocolateyPackageFolder "spring-1.5.22.RELEASE"
 
 Uninstall-ChocolateyEnvironmentVariable "SPRING_HOME" Machine
-#Uninstall-BinFile "spring" "$spring_home\bin\spring.bat"
+Uninstall-BinFile "spring" "$spring_home\bin\spring.bat"
