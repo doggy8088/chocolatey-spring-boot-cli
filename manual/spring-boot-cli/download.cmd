2d1be4a1aabc6883d13cf3c92aead0b4802a26b8
Paul Galbraith <paul.d.galbraith@gmail.com>
2020-01-18 12:01:59 -0500

    spring-boot-cli 2.0.8

--- manual/spring-boot-cli/download.cmd
+++ manual/spring-boot-cli/download.cmd
@@ -1,2 +1,2 @@
-curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.0.7.RELEASE/spring-boot-cli-2.0.7.RELEASE-bin.zip --output spring-boot-cli-2.0.7.RELEASE-bin.zip
-7z x spring-boot-cli-2.0.7.RELEASE-bin.zip
+curl https://repo.spring.io/release/org/springframework/boot/spring-boot-cli/2.0.8.RELEASE/spring-boot-cli-2.0.8.RELEASE-bin.zip --output spring-boot-cli-2.0.8.RELEASE-bin.zip
+7z x spring-boot-cli-2.0.8.RELEASE-bin.zip
--- manual/spring-boot-cli/tools/chocolateyInstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.7.RELEASE"
+$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.8.RELEASE"
 
 Install-ChocolateyEnvironmentVariable "SPRING_HOME" "$spring_home" Machine
 Install-BinFile "spring" "$spring_home\bin\spring.bat"
