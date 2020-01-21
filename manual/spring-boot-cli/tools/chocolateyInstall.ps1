b8c6db367c540915ea29052920bfac4f77997eeb
Paul Galbraith <paul.d.galbraith@gmail.com>
2020-01-21 14:21:10 -0500

    spring-boot-cli 2.2.4

--- manual/spring-boot-cli/tools/chocolateyInstall.ps1
+++ manual/spring-boot-cli/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 $toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.0.9.RELEASE"
+$spring_home = Join-Path $env:ChocolateyPackageFolder "spring-2.2.4.RELEASE"
 
 Install-ChocolateyEnvironmentVariable "SPRING_HOME" "$spring_home" Machine
 Install-BinFile "spring" "$spring_home\bin\spring.bat"
